40f475e5532a7c04e8b9db16d24db8e727415975
Ismael Haro <ismaharo18@gmail.com>
2017-12-09 11:46:58 -0600

    Añadido notificaciones

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,39 +1,44 @@
 var geo = {
 	exito: function(position){
-		var coordenadasHotel = {
-			lat: 19.054802,
-			lng: -98.225602
-		};
+		try{
+			var coordenadasHotel = {
+				lat: 19.054802,
+				lng: -98.225602
+			};
 
-		var coordenadasUsuario = {
-			lat:  position.coords.latitude,
-			lng:  position.coords.longitude
-		};
+			var coordenadasUsuario = {
+				lat:  position.coords.latitude,
+				lng:  position.coords.longitude
+			};
 
-		var map = new google.maps.Map(document.getElementById('mapa'), {
-        	zoom: 14,
-        	center: coordenadasUsuario
-        });
+			var map = new google.maps.Map(document.getElementById('mapa'), {
+	        	zoom: 14,
+	        	center: coordenadasUsuario
+	        });
 
-		var directionsDisplay = new google.maps.DirectionsRenderer({
-		    map: map
-		});
+			var directionsDisplay = new google.maps.DirectionsRenderer({
+			    map: map
+			});
 
-		var request = {
-			destination: coordenadasHotel,
-		    origin: coordenadasUsuario,
-		    travelMode: google.maps.TravelMode.DRIVING
-		};
+			var request = {
+				destination: coordenadasHotel,
+			    origin: coordenadasUsuario,
+			    travelMode: google.maps.TravelMode.DRIVING
+			};
 
-		  // Pass the directions request to the directions service.
-		var directionsService = new google.maps.DirectionsService();
-		
-		directionsService.route(request, function(response, status) {
-		    if (status == google.maps.DirectionsStatus.OK) {
-		      // Display the route on the map.
-		      directionsDisplay.setDirections(response);
-		    }
-		});
+			  // Pass the directions request to the directions service.
+			var directionsService = new google.maps.DirectionsService();
+			
+			directionsService.route(request, function(response, status) {
+			    if (status == google.maps.DirectionsStatus.OK) {
+			      // Display the route on the map.
+			      directionsDisplay.setDirections(response);
+			    }
+			});
+
+		}catch(error){
+			alert("Error obteniendo ubicacion: "+error);
+		}
 	},
 	error: function(error){
 		alert("ERROR: "+ error.message);
